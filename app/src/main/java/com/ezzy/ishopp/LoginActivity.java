cc37a10ebd2b3a42019c8bb2215e10212dae4025
Daniel Waiguru <52042764+DanielWaiguru91@users.noreply.github.com>
2020-10-14 08:32:45 +0300

    Fix activities transition loop (#28)
    * Adding Firebase Authentication Changes (#9)
    
    * update
    
    * Completed Firebase authentication
    
    * Completed Firebase authentication
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * feat: add bottom navigation view
    
    * feat: Add Navigation Tabs
    
    * feat: Add Option Menu
    
    * Fix: Fixed Computer Fragment
    
    * Update (#26)
    
    * update
    
    * Completed Firebase authentication
    
    * Completed Firebase authentication
    
    * Corrected session
    
    * Progressbar inserted
    
    * Fix merge conflicts (#18)
    
    * Adding Firebase Authentication Changes (#9)
    
    * update
    
    * Completed Firebase authentication
    
    * Completed Firebase authentication
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * feat: add bottom navigation view
    
    * feat: Add Navigation Tabs
    
    * feat: Add Option Menu
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * Develop (#19)
    
    * Adding Firebase Authentication Changes (#9)
    
    * update
    
    * Completed Firebase authentication
    
    * Completed Firebase authentication
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * feat: add bottom navigation view
    
    * feat: Add Navigation Tabs
    
    * feat: Add Option Menu
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * Fix Computer Fragment Instance (#20)
    
    * Adding Firebase Authentication Changes (#9)
    
    * update
    
    * Completed Firebase authentication
    
    * Completed Firebase authentication
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * feat: add bottom navigation view
    
    * feat: Add Navigation Tabs
    
    * feat: Add Option Menu
    
    * Fix: Fixed Computer Fragment
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * vinnie
    
    * Update
    
    * add progress and signout fuctionality
    
    * add progress and signout fuctionality(#22)(#23)
    
    * bottomnavigation drawer and its click listeners
    
    * Back Button issue
    
    * Back Button issue
    
    Co-authored-by: Ezekiel Wachira <48563395+EzekielWachira@users.noreply.github.com>
    Co-authored-by: Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>
    Co-authored-by: Vinnie-ndesh <54527486+Vinnie-ndesh@users.noreply.github.com>
    
    * fix: change bottom navigation item text color
    
    * Fix item text color in bottom navigation
    
    * Update
    
    * fix: authentication activities transition loop
    
    * fix: MainActivity onBackPress
    
    Co-authored-by: Ezekiel Wachira <48563395+EzekielWachira@users.noreply.github.com>
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    Co-authored-by: Ezekiel Wachira <ezekielwachira048@gmail.com>
    Co-authored-by: Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>
    Co-authored-by: Vinnie-ndesh <54527486+Vinnie-ndesh@users.noreply.github.com>

--- app/src/main/java/com/ezzy/ishopp/LoginActivity.java
+++ app/src/main/java/com/ezzy/ishopp/LoginActivity.java
@@ -99,6 +99,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(registerIntent);
+                //finish();
             }
         });
 
