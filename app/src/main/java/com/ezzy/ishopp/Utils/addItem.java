c8d5b6186f7ab5b1834f0aa92016342dc35f4ffc
Ezekiel Wachira <48563395+EzekielWachira@users.noreply.github.com>
2020-10-15 13:42:48 +0300

    Developer (#37)
    * Vender Registration (#34) (#35)
    
    * Back Button issue
    
    * Back Button issue
    
    * Vender Registration Added
    
    Co-authored-by: Ezekiel Wachira <48563395+EzekielWachira@users.noreply.github.com>
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    
    * vendor page (#30)
    
    * vendor page (#30)
    
    Co-authored-by: Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
    Co-authored-by: Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>

---
+++ app/src/main/java/com/ezzy/ishopp/Utils/addItem.java
@@ -0,0 +1,26 @@
+package com.ezzy.ishopp.Utils;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.ezzy.ishopp.R;
+
+public class addItem extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.add_vendor,null);
+builder.setView(view);
+        return builder.create();
+    }
+
+}
