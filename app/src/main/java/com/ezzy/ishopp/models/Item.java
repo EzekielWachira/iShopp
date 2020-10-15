db90698164f2d0404ddd43f720f66ceaa1a7a30a
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-10-15 17:05:13 +0300

    feat: save vendor details to database

---
+++ app/src/main/java/com/ezzy/ishopp/models/Item.java
@@ -0,0 +1,82 @@
+package com.ezzy.ishopp.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable {
+    private String name;
+    private String price;
+    private String description;
+    private String image_url;
+
+    public Item(String name, String price, String description, String image_url) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image_url = image_url;
+    }
+
+    protected Item(Parcel in) {
+        name = in.readString();
+        price = in.readString();
+        description = in.readString();
+        image_url = in.readString();
+    }
+
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(price);
+        dest.writeString(description);
+        dest.writeString(image_url);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
