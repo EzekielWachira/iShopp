eb4212298ddf995e1c6d2c11388474a845c68eba
Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>
2020-10-15 08:12:04 +0300

    vendor page (#30)

--- app/src/main/java/com/ezzy/ishopp/MainActivity.java
+++ app/src/main/java/com/ezzy/ishopp/MainActivity.java
@@ -112,6 +112,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             alertDialog.show();
 
         }
+        if (id==R.id.actionvendorpage){
+            Intent intent= new Intent(getApplicationContext(),VendorPage.class);
+            startActivity(intent);
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -136,12 +140,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(GravityCompat.START);
         }else {
-
             Intent a = new Intent(Intent.ACTION_MAIN);
             a.addCategory(Intent.CATEGORY_HOME);
-
             a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
             startActivity(a);
         }
 
