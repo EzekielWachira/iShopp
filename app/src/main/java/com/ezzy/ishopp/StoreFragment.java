09119262ed05bd557b207e0fdcd97a7754ec8e33
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-10-18 13:28:27 +0300

    feat: Add Store Adapter

--- app/src/main/java/com/ezzy/ishopp/StoreFragment.java
+++ app/src/main/java/com/ezzy/ishopp/StoreFragment.java
@@ -2,11 +2,27 @@ package com.ezzy.ishopp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.ezzy.ishopp.firebase.StoreAdapter;
+import com.ezzy.ishopp.models.Item;
+import com.ezzy.ishopp.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +30,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class StoreFragment extends Fragment {
+
+    private RecyclerView storeRecyclerview;
+    StoreAdapter adapter;
+    DatabaseReference mDatabaseReference;
+    FirebaseDatabase mDatabase;
+    List<Item> itemList;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,15 +50,6 @@ public class StoreFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static StoreFragment newInstance(String param1, String param2) {
         StoreFragment fragment = new StoreFragment();
         Bundle args = new Bundle();
@@ -58,7 +71,30 @@ public class StoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        View view = inflater.inflate(R.layout.fragment_store, container, false);
+        storeRecyclerview = view.findViewById(R.id.storeRecyclerView);
+//        itemList = new ArrayList<>();
+//        mDatabase = FirebaseDatabase.getInstance();
+//        mDatabaseReference = mDatabase.getReference();
+//        mDatabaseReference.child("store")
+//                .addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                            itemList = (List<Item>) dataSnapshot.getValue(User.class);
+//
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//                        makeToast("Error retrieving data from database");
+//                    }
+//                });
+        return view;
+    }
+
+    private void makeToast(String message){
+        Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
