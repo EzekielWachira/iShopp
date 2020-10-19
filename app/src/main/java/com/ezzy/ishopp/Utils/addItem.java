81c61a4fef96aa24ff40df3c38ca411a17e5348b
Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>
2020-10-19 15:01:02 +0300

    load vendor data (#46)&(#47)

--- app/src/main/java/com/ezzy/ishopp/Utils/addItem.java
+++ app/src/main/java/com/ezzy/ishopp/Utils/addItem.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +24,14 @@ import com.ezzy.ishopp.R;
 import com.ezzy.ishopp.models.Item;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class addItem extends DialogFragment {
 
@@ -40,14 +43,17 @@ public class addItem extends DialogFragment {
     private DatabaseReference mDatabaseReference;
     private static final int picId = 1;
     private static final int SELECT_FILE_ID = 12;
+    private String mCurrentUserId;
+    private DatabaseReference mRootref;
+    private Uri mFileUri;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.add_vendor,null);
-
+        View view = inflater.inflate(R.layout.add_vendor, null);
+        mRootref = FirebaseDatabase.getInstance().getReference();
         openCamera = view.findViewById(R.id.openCamera);
         openGallery = view.findViewById(R.id.openGallery);
         mItemName = view.findViewById(R.id.edittextitemName);
@@ -55,7 +61,7 @@ public class addItem extends DialogFragment {
         mItemPrice = view.findViewById(R.id.edittextitemprice);
         mSaveButton = view.findViewById(R.id.saveButton);
         mItemImage = view.findViewById(R.id.itemImage);
-
+mCurrentUserId= FirebaseAuth.getInstance().getCurrentUser().getUid();
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,12 +92,12 @@ public class addItem extends DialogFragment {
         return builder.create();
     }
 
-    private void openCamera(){
+    private void openCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, picId);
     }
 
-    private void openGallery(){
+    private void openGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
         startActivityForResult(galleryIntent, SELECT_FILE_ID);
@@ -100,30 +106,42 @@ public class addItem extends DialogFragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == picId && resultCode == Activity.RESULT_OK){
-            Bitmap image = (Bitmap)data.getExtras().get("data");
+        if (requestCode == picId && resultCode == Activity.RESULT_OK) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
             mItemImage.setImageBitmap(image);
+            mFileUri = data.getData();
         }
-        if (requestCode == SELECT_FILE_ID && resultCode == Activity.RESULT_OK){
+        if (requestCode == SELECT_FILE_ID && resultCode == Activity.RESULT_OK) {
             Uri selectedImageUri = data.getData();
             mItemImage.setImageBitmap(BitmapFactory.decodeFile(String.valueOf(selectedImageUri)));
+          mFileUri = data.getData();
         }
+        final StorageReference mItemStorageRef = FirebaseStorage.getInstance().getReference().child("Items");
 
+        String pushid = mRootref.child(mCurrentUserId).child("items").push().getKey();
+mItemStorageRef.putFile(mFileUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+    @Override
+    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+        if (task.isSuccessful()){
+
+        }
+    }
+});
     }
 
-    private void saveItem(String name, String price, String description, String image_url){
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference();
+    private void saveItem(String name, String price, String description, String image_url) {
+
 
         Item mItem = new Item();
         mItem.setName(name);
-        mItem .setPrice(price);
+        mItem.setPrice(price);
         mItem.setDescription(description);
         mItem.setImage_url(image_url);
 
-        mDatabaseReference.child("store")
-                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+        mRootref.child("store")
+                .child(mCurrentUserId)
                 .child("items")
+                .child(mRootref.child(mCurrentUserId).child("items").push().getKey())
                 .setValue(mItem)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -139,7 +157,7 @@ public class addItem extends DialogFragment {
         });
     }
 
-    private void makeToast(String message){
+    private void makeToast(String message) {
         Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
